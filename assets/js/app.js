8c9007e707c8c62021305a2f3e1d88cfc5b4500f
zhanglina0902 <81438901@qq.com>
2018-05-21 17:11:35 +0800

    发布招聘列表

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,22 @@
 // make console.log safe to use
 window.console||(console={log:function(){}});
 
-
+//Internet Explorer 10 in Windows 8 and Windows Phone 8 fix
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement('style');
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      '@-ms-viewport{width:auto!important}'
+    )
+  );
+  document.querySelector('head').appendChild(msViewportStyle)
+};
 
 
 
 //doc ready function
 $(document).ready(function() {
+
 
  	//Disable certain links
     $('a[href^=#]').click(function (e) {
